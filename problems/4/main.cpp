3624911e8825373335654889e6ee37d20673b62b
LyCecilion <LyCecilion@outlook.com>
2026-07-03 09:20:01 +0800

    fix(4): update solution for 命令行选项

--- problems/4/main.cpp
+++ problems/4/main.cpp
@@ -1,5 +1,10 @@
+/* 命令行选项 for problem 4 on XDOJ by LyCecilion - C++ version */
+
 #include <bits/stdc++.h>
 using namespace std;
+
+using i64 = long long;
+using u64 = unsigned long long;
 
 #ifdef LOCAL
 #define debug(x) cerr << "[debug] " << #x << " = " << (x) << '\n'
@@ -7,37 +12,32 @@ using namespace std;
 #define debug(x) ((void)0)
 #endif
 
-int main(void)
-{
-    string line;
-    if (!getline(cin, line)) return 0;
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    istringstream iss(line);
     set<string> options;
 
     string token;
-    while (iss >> token)
-    {
-        if (!token.empty() && token.front() == '-')
+    while (cin >> token) {
+        if (token.front() == '-')
             options.insert(token);
     }
 
-    if (options.empty())
-    {
-        std::cout << "no\n";
+    if (options.empty()) {
+        cout << "no";
         return 0;
     }
 
     bool first = true;
-    for (const auto& opt : options)
-    {
-        if (!first)
+    for (const auto& opt : options) {
+        if (first) {
             first = false;
-        else
-            std::cout << ' ';
-        std::cout << opt;
+        } else {
+            cout << ' ';
+        }
+        cout << opt;
     }
 
-    std::cout << '\n';
     return 0;
 }
